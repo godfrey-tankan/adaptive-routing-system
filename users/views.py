755b1f7febb392e102998f952fb3beec8f8099a9
godfrey-tankan <godfreytantswakandeya@gmail.com>
2025-06-28 20:32:45 +0200

    feat: Add additional log entry for successful user login

--- users/views.py
+++ users/views.py
@@ -12,24 +12,6 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 CustomUser = get_user_model()
 
-# class RegisterUserView(generics.CreateAPIView):
-#     queryset = CustomUser.objects.all()
-#     serializer_class = UserRegistrationSerializer
-#     permission_classes = [permissions.AllowAny]
-
-#     def post(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         user = serializer.save()
-        
-#         refresh = RefreshToken.for_user(user)
-#         return Response({
-#             "user": UserDetailsSerializer(user).data,
-#             "access": str(refresh.access_token),
-#             "refresh": str(refresh),
-#         }, status=status.HTTP_201_CREATED)
-
-
 class RegisterUserView(APIView):
     permission_classes = [permissions.AllowAny]
     """
